5ad9633e5524dbffd25f4e3a20020fb9cd479b6f
Juliansyah Rifqi <42226797+juliansyahrifqi@users.noreply.github.com>
2020-01-18 19:44:10 +0700

    revisi

--- js/api.js
+++ js/api.js
@@ -107,7 +107,11 @@ function getKlubById() {
     var urlParams = new URLSearchParams(window.location.search);
     var idParam = urlParams.get("id");
     
-    fetchApi("https://api.football-data.org/v2/teams/" + idParam)
+    fetch("https://api.football-data.org/v2/teams/" + idParam, {
+        headers: new Headers({
+            'X-Auth-Token': api_token
+        })
+    })
     .then(status)
     .then(json)
     .then(function(data) {
